73da446fd7fa7b51e5908a60591682dfd6a3f10d
kavinda <kavindasr@icloud.com>
2020-12-29 04:39:08 +0530

    change host

--- setting.js
+++ setting.js
@@ -21,5 +21,5 @@ exports.dbConfig = ksr;
 exports.webSettings = {
     webport: process.env.port || 8000,
     protocol: "http",
-    host: "192.168.1.8",
+    host: "http://oosdeb-env.eba-44zh8xut.us-east-2.elasticbeanstalk.com/",
 };
